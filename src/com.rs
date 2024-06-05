e7418543d9afa1a4e0d5ca76b1b35ceb3b66985f
Amit Goren <animalfarm@mailfence.com>
2024-06-05 15:17:54 +0300

    feat: extend Com to 32 bits

--- src/com.rs
+++ src/com.rs
@@ -1,12 +1,12 @@
-use fixed::{FixedI16, Wrapping};
+use fixed::{FixedI16, FixedI32, Wrapping};
 use ndarray::{Array, Dimension, ShapeBuilder};
 use ring::rand::{self, SecureRandom};
 
 /// A fixed-point number that is used for communication (hence the name 'Com') and upon which cryptography is performed.
 /// The fixed-point number is represented using a 16-bit signed integer, with the number of bits used for the fractional part
 /// defined by the `FRACTION_BITS` constant.
-pub type Com = Wrapping<FixedI16<4>>;
-
+// pub type Com = Wrapping<FixedI16<4>>;
+pub type Com = Wrapping<FixedI32<2>>;
 
 pub(crate) fn sample<Sh: ShapeBuilder>(shape: Sh, rng: &dyn SecureRandom) -> Array<Com, Sh::Dim> {
     // TODO implement RandomlyConstructable to avoid copying
